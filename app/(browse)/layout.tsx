d358ac20d93a7ce568fc693f96b422dc96ef0236
Kevin Camacho <Kevincxv@outlook.com>
2024-01-01 12:53:07 -0500

    Started side bar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from 'react'
 import { NavBar } from './_components/navbar'
+import { Sidebar } from './_components/sidebar'
 
 export default function BrosweLayout({children}: {children: React.ReactNode}) {
   return (
     <>
         <NavBar />
         <div className='flex h-full pt-20'>
+            <Sidebar />
             {children}
         </div>
     </>
